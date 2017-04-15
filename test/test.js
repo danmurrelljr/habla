a79e6561343536b487b65b13729f83a389e0afb7
Dan Murrell Jr <danmurrelljr@gmail.com>
2017-04-15 08:28:43 -0500

    Add tests for localize() function.

---
+++ test/test.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+var habla = require('../index.js')
+
+describe('Habla', function() {
+	describe('#localize()', function() {
+		it('should return \'undefined\' when localize key not provided', function() {
+			var shouldReturnUndefined = habla.localize();
+			assert.equal(shouldReturnUndefined, undefined);
+		});
+	});
+
+	describe('#localize(key)', function() {
+		it('should return \'key\' when localized string libraries are not present', function() {
+			var shouldReturnKey = habla.localize("key");
+			assert.equal(shouldReturnKey, 'key');
+		})
+	});
+});
