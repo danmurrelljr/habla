c1a38e766c3521f7f058116e2a4aadb885ec80b4
Dan Murrell Jr <danmurrelljr@gmail.com>
2017-04-17 06:18:41 -0500

    Use constructor pattern instead and add additional tests for language library.

--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var assert = require('assert');
-var habla = require('../index.js')
+var Habla = require('../index.js')
+var habla = new Habla();
 
 describe('Habla', function() {
 	describe('#localize()', function() {
@@ -25,6 +26,15 @@ describe('Habla', function() {
 		});
 	});
 
+	describe('#localize(key, language)', function() {
+		it('should return \'This is a localized en string\' from enLibrary for \'key\' when language is \'en\'', function () {
+			var enLibrary = { 'key': 'This is a localized en string' };
+			habla.setLibrary(enLibrary, 'en');
+			var shouldReturnLocalizedEn = habla.localize('key', 'en');
+			assert.equal(shouldReturnLocalizedEn, 'This is a localized en string');
+		});
+	});
+
 	var defaultLibrary = {};
 
 	describe('#setDefaultLibrary(library)', function() {
@@ -33,5 +43,14 @@ describe('Habla', function() {
 			var shouldReturnDefaultLibrary = habla.defaultLibrary;
 			assert.equal(shouldReturnDefaultLibrary, defaultLibrary);
 		});
+	});
+
+	describe('#setLibrary(forLibrary, language)', function() {
+		it('should set library for \'en\' when called', function() {
+			var enLibrary = { 'key': 'This is a localized en string' };
+			habla.setLibrary(enLibrary, 'en');
+			var shouldReturnEnLibrary = habla.libraries['en'];
+			assert.equal(shouldReturnEnLibrary, enLibrary);
+		})
 	});
 });
